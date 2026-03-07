b79dafe34cbb5b76aaffde5c90c15e4225c3427e
Pham Khanh Nhan <nhanpk28122004@gmail.com>
2026-03-07 22:49:56 +0700

    [Update] Add base module bot

---
+++ bot-csharp/YacchoBot/Modules/BaseModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace YacchoBot.Modules;
+
+public abstract class BaseModule : InteractionModuleBase<SocketInteractionContext>
+{
+    protected SocketGuildUser Invoker => (SocketGuildUser)Context.User;
+    protected SocketGuild Guild => Context.Guild;
+    protected SocketGuildUser BotUser => Guild.CurrentUser;
+    protected async Task RespondError(string message)
+    {
+        await RespondAsync(message, ephemeral: true);
+    }
+}
--- bot-csharp/YacchoBot/Program.cs
+++ bot-csharp/YacchoBot/Program.cs
@@ -1,9 +1,63 @@
-﻿namespace YacchoBot;
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using DotNetEnv;
+using Microsoft.Extensions.DependencyInjection;
+using YacchoBot.Services;
+
+namespace YacchoBot;
 
 class Program
 {
-    static void Main(string[] args)
+    private IServiceProvider _services;
+
+    static async Task Main(string[] args) => await new Program().MainAsync();
+
+    public async Task MainAsync()
     {
-        Console.WriteLine("Hello, World!");
+        Env.Load();
+
+        var config = new DiscordSocketConfig
+        {
+            GatewayIntents = GatewayIntents.Guilds |
+                             GatewayIntents.GuildMembers |
+                             GatewayIntents.GuildMessages |
+                             GatewayIntents.MessageContent
+        };
+
+        var services = new ServiceCollection()
+            .AddSingleton(new DiscordSocketClient(config))
+            .AddSingleton(x => new InteractionService(
+                x.GetRequiredService<DiscordSocketClient>()))
+            .AddSingleton<InteractionHandler>()
+            .BuildServiceProvider();
+
+        _services = services;
+
+        var client = services.GetRequiredService<DiscordSocketClient>();
+
+        client.Log += Log;
+
+        string? token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+
+        if (string.IsNullOrEmpty(token))
+        {
+            Console.WriteLine("Token not found!");
+            return;
+        }
+
+        await services.GetRequiredService<InteractionHandler>()
+            .InitializeAsync();
+
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg);
+        return Task.CompletedTask;
     }
 }
